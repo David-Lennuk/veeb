85e43ef3eba0bf7c3de4f01d9c51bbdd6f116d44
opilane <opilane@HP-CZC2349HT3>
2025-09-24 15:44:13 +0300

    Add project files.

---
+++ Controllers/PrimitiividController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace veeb.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PrimitiividController : ControllerBase
+    {
+
+        // GET: primitiivid/hello-world
+        [HttpGet("hello-world")]
+        public string HelloWorld()
+        {
+            return "Hello world at " + DateTime.Now;
+        }
+
+    }
+}
